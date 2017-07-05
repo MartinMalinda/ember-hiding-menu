f99be108ee34c84398d76b76cc24675e74e14605
martinmalinda <malindacz@gmail.com>
2017-07-06 00:00:08 +0200

    update deps

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -4,13 +4,12 @@ import config from '../../config/environment';
 import './animate-scroll';
 
 export default function startApp(attrs) {
-  let application;
 
   let attributes = Ember.merge({}, config.APP);
   attributes = Ember.merge(attributes, attrs); // use defaults, but you can override;
 
-  Ember.run(() => {
-    application = Application.create(attributes);
+  return Ember.run(() => {
+    let application = Application.create(attributes);
     application.setupForTesting();
     application.injectTestHelpers();
   });
--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,6 +1,8 @@
 import resolver from './helpers/resolver';
+import { start } from 'ember-cli-qunit';
 import {
   setResolver
 } from 'ember-qunit';
 
 setResolver(resolver);
+start();
